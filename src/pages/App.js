89dd985489eac44cda42aa4e348b3f6b82a0eb34
Alexduanran <alexduanran@gmail.com>
2020-11-26 14:47:47 -0500

    Page transition working

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,23 +2,31 @@ import css from './App.module.css';
 import Home from './Home'
 import Genetic from './Genetic'
 import {
-  BrowserRouter as Router,
+  Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+import { createBrowserHistory } from 'history';
 
+const history = createBrowserHistory();
 
 function App() {
   return (
     <div>
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router history={history}>
         <Switch>
-          <Route path='/'>
+          <Route path='/snake'>
             <Genetic />
+          </Route>
+          <Route path='/'>
+            <Home history={history}/>
           </Route>
         </Switch>
       </Router>
+      {/* <footer>
+        Copyright © 2020 AlphaGame
+      </footer> */}
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,105 @@
 import css from './Home.module.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Genetic from './Genetic'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Pagination, EffectCoverflow } from 'swiper';
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+import 'swiper/components/effect-coverflow/effect-coverflow.scss';
 
-function Home() {
-  return (
-    <div className={css.container}>
-        <h1 className={css.name}>
-            AlphaGame
-        </h1>
-        <div className={css.background}>
-            some
-        </div>
-    </div>
-  );
-}
+function Home(props) {
+
+    SwiperCore.use([EffectCoverflow, Pagination]);
+
+    function GameSwiper() {
+        return (
+            <Swiper effect='coverflow'
+                    spaceBetween={30}
+                    slidesPerView={3}
+                    pagination={{ clickable: true }}
+                    centeredSlides={true}
+                    loop={true}
+                    >
+                <SwiperSlide>
+                    <Link to='/snake'>
+                        <div className={css.square}>
+                            Snake
+                        </div>
+                    </Link>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Link to='/pong'>
+                        <div className={css.square}>
+                            Pong
+                        </div>
+                    </Link>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Link to='/flappybird'>
+                        <div className={css.square}>
+                            Flappy Bird
+                        </div>
+                    </Link>
+                </SwiperSlide>
+            </Swiper>
+        )
+    }
+
+    function GameModes(prop) {
+        return (
+            <div className={css.grid}>
+                <div className={css.smallsquare}>
+                    {prop.name}
+                </div>
+                <div className={css.smallsquare} onClick={()=>{props.history.push('/' + prop.name + '/genetic')}} >
+                    Genetic
+                </div>
+                <div className={css.smallsquare} onClick={()=>{props.history.push('/' + prop.name + '/reinforcement')}} >
+                    Reinforcement Learning
+                </div>
+                <div className={css.smallsquare} onClick={()=>{props.history.push('/' + prop.name + '/battle')}} >
+                    Battle
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <Router history={props.history}>
+            <div className={css.container}>
+                <h1 className={css.name}>
+                    <Link to='/'>
+                        AlphaGame
+                    </Link>
+                </h1>
+                <div className={css.background}>
+                    <div className={css.screen}>
+                        <Switch>
+                            <Route path='/snake' >
+                                <GameModes name={'snake'} />
+                            </Route>
+                            <Route path='/pong'>
+                                <GameModes name={'pong'} />
+                            </Route>
+                            <Route path='/flappybird'>
+                                <GameModes name={'flappybird'} />
+                            </Route>
+                            <Route path='/'>
+                                <GameSwiper className={css.swiper}/>
+                            </Route>
+                        </Switch>
+                    </div>
+                </div>
+            </div>
+        </Router>
+    );
+    }
 
 export default Home;
